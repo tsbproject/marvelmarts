2c851c6117644ae6bc9ee69eaf3a7d293bf474e9
TSB <122724396+tsbproject@users.noreply.github.com>
2025-11-19 14:49:26 +0100

    [commit]

--- app/_lib/prisma.ts
+++ app/_lib/prisma.ts
@@ -8,11 +8,17 @@ const globalForPrisma = globalThis as unknown as {
 export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
-    log: process.env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
+    datasources: {
+      db: {
+        url: process.env.DATABASE_URL, // ← add this line for Prisma 7+
+      },
+    },
+    log:
+      process.env.NODE_ENV === 'development'
+        ? ['query', 'error', 'warn']
+        : ['error'],
   });
 
 if (process.env.NODE_ENV !== 'production') {
   globalForPrisma.prisma = prisma;
 }
-
-
