3a443e5fc2a850a30345f80024cd2475803f12a4
TSB <122724396+tsbproject@users.noreply.github.com>
2025-11-24 14:25:24 +0100

    [commit]

--- prisma.config.js
+++ prisma.config.js
@@ -1,14 +1,10 @@
-// prisma.config.js
-
-/** @type {import('prisma').PrismaConfig} */
 module.exports = {
   schema: './prisma/schema.prisma',
 
- 
   datasource: {
     db: {
-      provider: 'postgresql',              
-      url: { env: 'DATABASE_URL' },       
+      provider: 'postgresql',
+      url: process.env.DATABASE_URL,
     },
   },
 };
