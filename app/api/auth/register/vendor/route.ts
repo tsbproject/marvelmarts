9277e933159383adc640d31f489b74dd71a50af5
TSB <122724396+tsbproject@users.noreply.github.com>
2025-11-30 07:55:32 +0000

    [commit]

--- app/api/auth/register/vendor/route.ts
+++ app/api/auth/register/vendor/route.ts
@@ -1,8 +1,8 @@
 import { NextResponse } from "next/server";
 import bcrypt from "bcrypt";
-import { PrismaClient } from "@prisma/client";
+import { prisma } from "@/app/lib/prisma"; 
 
-const prisma = new PrismaClient();
+
 
 export async function POST(req: Request) {
   try {
