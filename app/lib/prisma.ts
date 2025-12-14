7c2fd2bac5aa048f186777d2e2d4c2f0a787ee1f
TSB <122724396+tsbproject@users.noreply.github.com>
2025-12-14 21:33:22 +0100

    [commit]

--- app/lib/prisma.ts
+++ app/lib/prisma.ts
@@ -1,6 +1,3 @@
-
-
-
 // import { PrismaClient } from "@prisma/client";
 // import { PrismaNeon } from "@prisma/adapter-neon";
 
@@ -8,20 +5,22 @@
 //   connectionString: process.env.DATABASE_URL!,
 // });
 
-// const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient };
-
-// export const prisma =
-//   globalForPrisma.prisma ??
-//   new PrismaClient({
-//     adapter,
-//     log: ["query", "error", "warn"], // optional logging
-//   });
-
-// if (process.env.NODE_ENV !== "production") {
-//   globalForPrisma.prisma = prisma;
+// declare global {
+//   // prevent multiple instances during hot reload in dev
+//   var prisma: PrismaClient | undefined;
 // }
 
+// export const prisma: PrismaClient =
+//   global.prisma ??
+//   new PrismaClient({
+//     adapter,
+//     log: ["query", "error", "warn"],
+//   });
+
+// if (process.env.NODE_ENV !== "production") global.prisma = prisma;
+
 // export default prisma;
+
 
 import { PrismaClient } from "@prisma/client";
 import { PrismaNeon } from "@prisma/adapter-neon";
@@ -30,18 +29,18 @@ const adapter = new PrismaNeon({
   connectionString: process.env.DATABASE_URL!,
 });
 
-declare global {
-  // prevent multiple instances during hot reload in dev
-  var prisma: PrismaClient | undefined;
-}
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
-export const prisma: PrismaClient =
-  global.prisma ??
+export const prisma =
+  globalForPrisma.prisma ||
   new PrismaClient({
     adapter,
     log: ["query", "error", "warn"],
   });
 
-if (process.env.NODE_ENV !== "production") global.prisma = prisma;
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
 
 export default prisma;
+
