8bfd02f5fd60b677cd3b59c2e36ca436cb07f883
TSB <122724396+tsbproject@users.noreply.github.com>
2026-01-21 04:00:25 +0100

    [commit]

--- store/index.ts
+++ store/index.ts
@@ -1,13 +1,19 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./authSlice";
 import cartReducer from "./cartSlice"; 
-
+import productReducer from "./productSlice"; 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
-    cart: cartReducer, // Registering the cart reducer
+    cart: cartReducer,
+    products: productReducer, // 2. Add this line to match 'state.products'
   },
 });
+
+
+
+
+
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
