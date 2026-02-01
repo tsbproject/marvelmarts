3fb0f877df1b8492095778b589dfa3bf47dffd3d
TSB <122724396+tsbproject@users.noreply.github.com>
2026-02-01 06:02:48 +0100

    [commit]

--- app/products/[slug]/loading.tsx
+++
@@ -1,8 +0,0 @@
-// app/products/[slug]/loading.tsx
-export default function Loading() {
-  return (
-    <div className="flex items-center justify-center min-h-screen bg-neutral-white">
-      <div className="w-12 h-12 border-4 border-brand-primary border-t-transparent rounded-full animate-spin"></div>
-    </div>
-  );
-}
