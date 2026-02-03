3b254c842524f9a1c20191a01c61ce94b0e18dd0
TSB <122724396+tsbproject@users.noreply.github.com>
2026-02-03 23:59:34 +0100

    [commit]

--- app/api/admins/reviews/[id]/route.ts
+++ app/api/admins/reviews/[id]/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import { prisma } from "@/app/lib/prisma";
 import { getServerSession } from "next-auth";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { authOptions } from "@/app/lib/auth";
 
 /**
  * PATCH: Update Review Status (Approve/Unapprove)
