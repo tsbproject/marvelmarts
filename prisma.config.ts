50d8a4127c1060f7e369566648b770dbafd6c0cd
TSB <122724396+tsbproject@users.noreply.github.com>
2026-04-06 23:38:55 +0100

    [commit]

--- prisma.config.ts
+++ prisma.config.ts
@@ -15,3 +15,24 @@ export default defineConfig({
 
 
 
+// import { PrismaClient } from "@prisma/client";
+// import { PrismaPg } from "@prisma/adapter-pg";
+
+// const globalForPrisma = globalThis as unknown as {
+//   prisma: PrismaClient | undefined;
+// };
+
+// const adapter = new PrismaPg({
+//   connectionString: process.env.DATABASE_URL!,
+// });
+
+// export const prisma =
+//   globalForPrisma.prisma ??
+//   new PrismaClient({
+//     adapter,
+//     log: ["error", "warn"],
+//   });
+
+// if (process.env.NODE_ENV !== "production") {
+//   globalForPrisma.prisma = prisma;
+// }
