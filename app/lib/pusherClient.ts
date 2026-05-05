32c093dc002c969eae1af49fd4506952d5185c54
TSB <122724396+tsbproject@users.noreply.github.com>
2026-05-05 16:59:18 +0100

    [commit]

--- app/lib/pusherClient.ts
+++ app/lib/pusherClient.ts
@@ -1,11 +1,27 @@
-import PusherClient from "pusher-js";
+// import PusherClient from "pusher-js";
 
+
+// // Ensure these match exactly what you named in your .env
+// export const pusherClient = new PusherClient(
+//   process.env.NEXT_PUBLIC_PUSHER_KEY!,
+//   {
+//     cluster: process.env.NEXT_PUBLIC_PUSHER_CLUSTER!,
+//     forceTLS: true, // Recommended for security
+//   }
+// );
+
+
+
+
+"use client";
+
+import Pusher from "pusher-js";
 
 // Ensure these match exactly what you named in your .env
-export const pusherClient = new PusherClient(
+export const pusherClient = new Pusher(
   process.env.NEXT_PUBLIC_PUSHER_KEY!,
   {
     cluster: process.env.NEXT_PUBLIC_PUSHER_CLUSTER!,
-    forceTLS: true, // Recommended for security
+    forceTLS: true,
   }
 );
