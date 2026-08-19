e039e0327ec260cbfd4fdf8609808301c35978a3
TSB <122724396+tsbproject@users.noreply.github.com>
2026-08-19 16:20:24 +0100

    [commit]

--- app/lib/prisma.ts
+++ app/lib/prisma.ts
@@ -1,60 +1,62 @@
 //NEON DATABASE
 
 
-import { PrismaClient } from "@prisma/client";
-import { PrismaNeon } from "@prisma/adapter-neon";
+// import { PrismaClient } from "@prisma/client";
+// import { PrismaNeon } from "@prisma/adapter-neon";
 
-const adapter = new PrismaNeon({
-  connectionString: process.env.DATABASE_URL!,
-});
+// const adapter = new PrismaNeon({
+//   connectionString: process.env.DATABASE_URL!,
+// });
 
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
+// const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
-export const prisma =
-  globalForPrisma.prisma ||
-  new PrismaClient({
-    adapter,
-    log: ["query", "error", "warn"],
-  });
+// export const prisma =
+//   globalForPrisma.prisma ||
+//   new PrismaClient({
+//     adapter,
+//     log: ["query", "error", "warn"],
+//   });
 
-if (process.env.NODE_ENV !== "production") {
-  globalForPrisma.prisma = prisma;
-}
+// if (process.env.NODE_ENV !== "production") {
+//   globalForPrisma.prisma = prisma;
+// }
 
-export default prisma;
+// export default prisma;
+
+
+
+
 
 
 //PRISMA DATABASE
 
-// import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '@prisma/client'
 
-// import { PrismaPg } from '@prisma/adapter-pg'
-// import { Pool } from 'pg'
+import { PrismaPg } from '@prisma/adapter-pg'
+import { Pool } from 'pg'
 
-// const pool = new Pool({
-//   connectionString: process.env.DATABASE_URL!,
-// })
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL!,
+})
 
-// const adapter = new PrismaPg(pool)
+const adapter = new PrismaPg(pool)
 
-// const globalForPrisma = globalThis as unknown as {
-//   prisma?: PrismaClient
-// }
+const globalForPrisma = globalThis as unknown as {
+  prisma?: PrismaClient
+}
 
-// export const prisma =
-//   globalForPrisma.prisma ??
-//   new PrismaClient({
-//     adapter,
-//     log: ['query', 'error', 'warn'],
-//   })
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    adapter,
+    log: ['query', 'error', 'warn'],
+  })
 
-// if (process.env.NODE_ENV !== 'production') {
-//   globalForPrisma.prisma = prisma
-// }
+if (process.env.NODE_ENV !== 'production') {
+  globalForPrisma.prisma = prisma
+}
 
-// export default prisma
-
-
+export default prisma
 
 
 
@@ -64,20 +66,22 @@ export default prisma;
 
 
 
-export async function getSiteSettings() {
-  return prisma.siteSettings.findFirst() ?? {
+
+
+// export async function getSiteSettings() {
+//   return prisma.siteSettings.findFirst() ?? {
  
-  accentNavy: '#002B5B',
-  brandPrimary:' #F7931E',
-  brandGhost: '#1f2937',
-  neutralWhite : '#d1d5db',
-  neutralLight: '#f3f4f6',
-  neutralDark: '#1f2937',
-  brandOrangeLight: '#FFE8CC', 
-  neutralGray : '#4B4B4B'      
+//   accentNavy: '#002B5B',
+//   brandPrimary:' #F7931E',
+//   brandGhost: '#1f2937',
+//   neutralWhite : '#d1d5db',
+//   neutralLight: '#f3f4f6',
+//   neutralDark: '#1f2937',
+//   brandOrangeLight: '#FFE8CC', 
+//   neutralGray : '#4B4B4B'      
  
   
  
    
-  };
-}
+//   };
+// }
